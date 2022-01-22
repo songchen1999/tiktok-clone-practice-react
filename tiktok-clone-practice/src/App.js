e9e922e002bbd3d00f99ff241688f85943900650
Song Chen <43616275+songchen1999@users.noreply.github.com>
2022-01-21 20:13:18 -0500

    init

---
+++ tiktok-clone-practice/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Video from './Video';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>fuck me</h1>
+
+     <div className="app_videos">
+       <Video/> 
+       <Video/> 
+       <Video/> 
+       <Video/> 
+
+     </div>
+
+    </div>
+  );
+}
+
+export default App;
